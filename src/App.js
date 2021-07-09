ee2fd50e71f121504301c54758a36fe02d4fa76d
David-V-Rullo <d.v.rullo@gmail.com>
2021-07-09 12:21:31 -0400

    Working on React skeleton

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import PageHead from "./Components/PageHead"
+import Navigation from './Components/Navigation';
+
 
 function App() {
   return (
+    <div>
+    <PageHead />
+    <Navigation />
     <Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
     </Router>
+    </div>
   );
 }
 
